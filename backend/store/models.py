c02fd71b51a89a9e24112bc4dcd59fddef8b6d4b
yankkv17 <yankkv17@gmail.com>
2024-04-19 23:42:19 +0300

    fix: Update __str__ method in ProductImage model
    Added a __str__ method to the ProductImage model to return the name of
    the associated product, improving readability and usability.

--- backend/store/models.py
+++ backend/store/models.py
@@ -26,6 +26,9 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.product.name}'
+
 
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
