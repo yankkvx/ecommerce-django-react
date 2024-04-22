e1fd36e08c1fdb781cbc387dc6eefa08e2ba4ad2
yankkv17 <yankkv17@gmail.com>
2024-04-22 21:53:17 +0300

    feat: Implement cart item removal functionality
    Added functionality to cartReducers and cartConstants to enable the
    removal of items from the cart.

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,23 +3,34 @@ import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 // getState let us get any part of the state
 export const addToCart = (id, quantity) => async (dispatch, getState) => {
-    try {
-        const { data } = await axios.get(`/api/products/${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
-        dispatch({
-            type: CART_ADD_ITEM,
-            payload: {
-                product: data.id,
-                name: data.name,
-                image: data.images[0],
-                price: data.price,
-                count_in_stock: data.count_in_stock,
-                quantity,
-            },
-        });
-        localStorage.setItem(
-            "cartItems",
-            JSON.stringify(getState().cart.cartItems)
-        );
-    } catch (error) {}
+    dispatch({
+        type: CART_ADD_ITEM,
+        payload: {
+            product: data.id,
+            name: data.name,
+            image: data.images[0],
+            price: data.price,
+            count_in_stock: data.count_in_stock,
+            quantity,
+        },
+    });
+
+    localStorage.setItem(
+        "cartItems",
+        JSON.stringify(getState().cart.cartItems)
+    );
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
+    });
+
+    localStorage.setItem(
+        "cartItems",
+        JSON.stringify(getState().cart.cartItems)
+    );
 };
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,29 +1,37 @@
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
-
-export const cartReducer = (state={cartItems: []}, action) => {
-    switch(action.type){
+export const cartReducer = (state = { cartItems: [] }, action) => {
+    switch (action.type) {
         case CART_ADD_ITEM:
-            const item = action.payload
+            const item = action.payload;
             // Check if the item is already in the cart.
-            const existItem = state.cartItems.find(x => x.product === item.product)
+            const existItem = state.cartItems.find(
+                (x) => x.product === item.product
+            );
             // If the item already exist in the cart.
-            if (existItem){
+            if (existItem) {
                 // Return the new state with updated product quantity.
                 return {
                     ...state,
-                    cartItems: state.cartItems.map(x => x.product === existItem.product ? item : x
-                    )
-                }
+                    cartItems: state.cartItems.map((x) =>
+                        x.product === existItem.product ? item : x
+                    ),
+                };
             } else {
                 // If the item doesnt already exist in the cart add it and return the new state with the added product
                 return {
                     ...state,
-                    cartItems:[...state.cartItems, item]
-                }
+                    cartItems: [...state.cartItems, item],
+                };
             }
 
+        case CART_REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems:state.cartItems.filter(x => x.product !== action.payload)
+            };
+
         default:
-            return state
-    }    
-}
+            return state;
+    }
+};
