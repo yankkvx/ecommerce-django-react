d28832e8cd1156af0facdaa6cb002f06a7389d30
yankkv17 <yankkv17@gmail.com>
2024-04-24 23:27:06 +0300

    feat: Install and customize JWT

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.get_products, name='products'),
     path('products/<int:pk>/', views.get_product, name='product'),
 ]
