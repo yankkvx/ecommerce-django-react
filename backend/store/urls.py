c8a287e95efca72550b4532b47e863cbebb78f3a
yankkv17 <yankkv17@gmail.com>
2024-04-29 23:26:31 +0300

    feat: Update views and apply permissions for route protection
    Improved views to include necessary logic for route protection and added
    permissions to safeguard routes.

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('products/', views.get_products, name='products'),
     path('products/<int:pk>/', views.get_product, name='product'),
+    path('users/', views.get_users, name='users'),
     path('users/profile/', views.get_user, name='user-profile'),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Product
 from django.contrib.auth.models import User
 from .serializers import ProductSerializer, UserSerializer, UserSerializerRefreshToken
@@ -21,9 +22,18 @@ class MyTokenObtainPairView(TokenObtainPairView):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_user(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
