29f69199f9a0ab8fcca9a187ee2cdea393adc076
yankkv17 <yankkv17@gmail.com>
2024-05-02 23:11:15 +0300

    feat: Integrate userReducers into store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,11 +7,13 @@ import {
     singleProductReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
     singleProduct: singleProductReducer,
     cart: cartReducer,
+    userLogin: userLoginReducer,
 });
 
 // Load data into state
@@ -19,9 +21,13 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : [];
 
 const initialState = {
     cart: { cartItems: cartItemsFromStorage },
+    userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
