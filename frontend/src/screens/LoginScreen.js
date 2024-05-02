64518cd62e77f318e7cf29acb9c62c827c4d2f38
yankkv17 <yankkv17@gmail.com>
2024-05-02 23:08:22 +0300

    feat: Create LoginScreen and FormContainer components
    Implemented LoginScreen component to display the login form and
    facilitate user login functionality. Created FormContainer component to
    serve as a reusable container for forms, which will be utilized in both
    LoginScreen and RegisterScreen components.

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import FormContainer from "../components/FormContainer";
+import { login } from "../actions/userActions";
+
+function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const location = useLocation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const redirect = new URLSearchParams(window.location.search).get("redirect");
+    const userLogin = useSelector((state) => state.userLogin);
+    const { error, loading, userInfo } = userLogin;
+
+    useEffect(() => {
+        // Check if userInfo is defined and not null or undefined.
+        if (userInfo && Object.keys(userInfo).length !== 0) {
+            // If we have userInfo and it's not empty proceed.
+            // If the "redirect" parameter is not specified, I'll be redirected to the home page ("/").
+            navigate(redirect || "/");
+        }
+    }, [userInfo, navigate]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(login(email, password));
+    };
+
+    return (
+        <FormContainer>
+            <h1>Sign In</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="danger">{error}</Message>
+            ) : (
+                <Form onSubmit={submitHandler}>
+                    <Form.Group className="py-2" controlId="email">
+                        <Form.Label>Email Address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter your email address"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+
+                    <Form.Group className="py-2" controlId="password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Enter your password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        ></Form.Control>
+                    </Form.Group>
+                    <div className="py-2 text-center">
+                        <Button
+                            className="rounded"
+                            type="submit"
+                            variant="primary"
+                        >
+                            Sign In
+                        </Button>
+                    </div>
+                </Form>
+            )}
+
+            <Row>
+                <Col className="text-center">
+                    New to Frame?{" "}
+                    <Link
+                        to={
+                            redirect
+                                ? `/register?redirect=${redirect}`
+                                : "/register"
+                        }
+                        className="text-link"
+                    >
+                        Sign Up
+                    </Link>
+                </Col>
+            </Row>
+        </FormContainer>
+    );
+}
+
+export default LoginScreen;
