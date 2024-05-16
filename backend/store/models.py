63529920340cc3acea6c4c1492170e997c107d42
yankkv17 <yankkv17@gmail.com>
2024-05-16 19:29:40 +0300

    fix: Update Order model fields to use DateTimeField

--- backend/store/models.py
+++ backend/store/models.py
@@ -49,10 +49,10 @@ class Order(models.Model):
     total_price = models.DecimalField(
         max_digits=10, decimal_places=2, null=True, blank=True)
     is_paid = models.BooleanField(default=False)
-    paid_at = models.DateField(auto_now_add=False, null=True, blank=True)
+    paid_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
     is_delivered = models.BooleanField(default=False)
-    delivered_at = models.DateField(auto_now_add=False, null=True, blank=True)
-    created_at = models.DateField(auto_now_add=True)
+    delivered_at = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.first_name + self.user.last_name} made an order №{self.id} at {self.created_at}'
