217630e385ece94a0196e8d4605be15389e445e6
yankkv17 <yankkv17@gmail.com>
2024-05-25 23:30:26 +0300

    feat: Create views and URLs for product creation and editing
    Developed views and URL endpoints to enable product creation and editing
    within the application.

--- backend/store/views/product_views.py
+++ backend/store/views/product_views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from ..models import Product
+from ..models import Product, ProductImage
 from django.contrib.auth.models import User
 from ..serializers import ProductSerializer
 from rest_framework import status
@@ -35,3 +35,48 @@ def delete_product(request, pk):
         return Response(content, status=status.HTTP_404_NOT_FOUND)
     except Exception as e:
         return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def create_product(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='Name',
+        brand='Brand',
+        category='Category',
+        description='',
+        price=0,
+        count_in_stock=0,
+    )
+
+    images = request.FILES.getlist('images')
+    for image in images:
+        ProductImage.objects.create(product=product, image=image)
+
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def edit_product(request, pk):
+    try:
+        data = request.data
+        product = Product.objects.get(id=pk)
+        product.name = data['name']
+        product.brand = data['brand']
+        product.category = data['category']
+        product.description = data['description']
+        product.price = data['price']
+        product.count_in_stock = data['count_in_stock']
+        product.save()
+        
+        serializer = ProductSerializer(product, many=False)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        content = {'detail': 'Product not found.'}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
