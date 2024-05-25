54733f4f1b26c2c1c7ff15ff3c7a707aa1b64ee0
yankkv17 <yankkv17@gmail.com>
2024-05-25 19:34:09 +0300

    feat: Implement view and URL to delete products
    Implemented functionality to delete products via a view and URL endpoint
    enabling administrators to remove products from the system.

--- backend/store/urls/product_urls.py
+++ backend/store/urls/product_urls.py
@@ -5,4 +5,5 @@ from store.views import product_views
 urlpatterns = [
     path('', product_views.get_products, name='products'),
     path('<int:pk>/', product_views.get_product, name='product'),
+    path('delete/<str:pk>/', product_views.delete_product, name='delete-product'),
 ]
--- backend/store/views/product_views.py
+++ backend/store/views/product_views.py
@@ -5,6 +5,7 @@ from ..models import Product
 from django.contrib.auth.models import User
 from ..serializers import ProductSerializer
 from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @api_view(['GET'])
@@ -19,3 +20,18 @@ def get_product(request, pk):
     product = Product.objects.get(id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_product(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+        product.delete()
+        content = {'detail': 'Product successfully deleted.'}
+        return Response(content, status=status.HTTP_200_OK)
+    except ObjectDoesNotExist:
+        content = {'detail': 'Product not found.'}
+        return Response(content, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
