11052a7d793910bed5a2c74deebc0821c2234e88
yankkv17 <yankkv17@gmail.com>
2024-06-01 20:56:10 +0300

    feat: Create SearchBox component
    Created the SearchBox component to allow users to search for products by
    entering keywords.

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+function SearchBar() {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if (query) {
+            navigate(`/?query=${query}`);
+        } else {
+            navigate(window.location.pathname);
+        }
+    };
+    return (
+        <Form onSubmit={submitHandler} className="d-flex">
+            <Form.Control
+                type="text"
+                name="q"
+                onChange={(e) => setQuery(e.target.value)}
+                className="mr-sm-2 p-1"
+            ></Form.Control>
+            <Button type="submit" variant="outline-primary" className="p-1">
+                <i className="fa-solid fa-magnifying-glass" />
+            </Button>
+        </Form>
+    );
+}
+
+export default SearchBar;
