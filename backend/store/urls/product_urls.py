b750e821fca87715a7878bacb0aa839deae0c15b
yankkv17 <yankkv17@gmail.com>
2024-06-26 23:26:35 +0300

    refactor: Refactor URL configuration to use class-based views
    Updated URL patterns in "urls.py" to utilize class-based views for
    better code organization and maintainability. Imported necessary views
    from the "product_views" module and renamed paths for clarity and
    consistency.

--- backend/store/urls/product_urls.py
+++ backend/store/urls/product_urls.py
@@ -1,21 +1,18 @@
 from django.urls import path
 from store.views import product_views
-
+from store.views.product_views import (
+    GetProducts, GetLatestProducts, ProductsByCategory, GetCategories, ProductDetails, ReviewDetails, FavouritesDetails)
 
 urlpatterns = [
-    path('', product_views.get_products, name='products'),
-    path('latest/', product_views.get_latest_products, name='last-products'),
-    path('create/', product_views.create_product, name='create-product'),
-    path('upload/', product_views.upload_image, name='image-ipload'),
-    path('<str:pk>/reviews/', product_views.post_review, name='post-review'),
-    path('delete/review/<str:pk>/', product_views.delete_review, name='delete-review'),
-    path('<int:pk>/', product_views.get_product, name='product'),
-    path('category/', product_views.get_categories, name='category'),
-    path('category/<str:category>/', product_views.products_by_category,
-         name='product-by-category'),
-    path('favourites/', product_views.get_favourites, name='favourites'),
-    path('<str:pk>/add-to-favourites/',  product_views.add_to_favourites, name='add-to-favourites'),
-    path('<str:pk>/remove-from-favourites/',  product_views.remove_from_favourites, name='remove-from-favourites'),
-    path('edit/<str:pk>/', product_views.edit_product, name='edit-product`'),
-    path('delete/<str:pk>/', product_views.delete_product, name='delete-product'),
+    path('', GetProducts.as_view(), name='products'),
+    path('latest/', GetLatestProducts.as_view(), name='last-products'),
+    path('create/', ProductDetails.as_view(), name='create-product'),
+    path('upload/', product_views.upload_image, name='image-upload'),
+    path('<int:pk>/', ProductDetails.as_view(), name='product'),
+    path('<int:pk>/action/', ProductDetails.as_view(), name='product-action'),
+    path('<str:pk>/review/', ReviewDetails.as_view(), name='post-review'),
+    path('category/', GetCategories.as_view(), name='category-list'),
+    path('category/<str:category>/', ProductsByCategory.as_view(), name='product-by-category'),
+    path('favourites/', FavouritesDetails.as_view(), name='favourites-list'),
+    path('<str:pk>/favourites/', FavouritesDetails.as_view(), name='add-to-favourites'),
 ]
