fc7e7f501225d99ba9b3214f80f4a2d04f3183c9
yankkv17 <yankkv17@gmail.com>
2024-06-27 17:46:59 +0300

    refactor: Update views to CBV for better structure and reusability
    Updated user management API views to utilize Django REST frameworks
    class-based views for improved structure and reusability. Implemented
    separate classes for handling general user operations and admin-specific
    tasks, such as user creation, updates, deletion, and retrieval.
    Enhanced error handling and response formatting to ensure clearer
    API endpoint behavior.

--- backend/store/views/user_views.py
+++ backend/store/views/user_views.py
@@ -1,15 +1,22 @@
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.contrib.auth.models import User
-from ..serializers import ProductSerializer, UserSerializer, UserSerializerRefreshToken
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.hashers import make_password
+from ..serializers import UserSerializer, UserSerializerRefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth.hashers import make_password
-from rest_framework import status
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    Custom serializer for JWT token obtain pair.
+
+    Extends the default token obtain pair serializer to include user data.
+    """
+
     def validate(self, attrs):
         data = super().validate(attrs)
         serialzier = UserSerializerRefreshToken(self.user).data
@@ -19,106 +26,119 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
+    """
+    Custom view for JWT token obtain pair.
+
+    Uses MyTokenObtainPairSerializer for validation.
+    """
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def update_user(request):
-    user = request.user
-    serializer = UserSerializerRefreshToken(user, many=False)
+class UserManagement(APIView):
+    """
+    API endpoint for managing user details.
 
-    data = request.data
-    user.first_name = data['first_name']
-    user.last_name = data['last_name']
-    user.email = data['email']
-    if data['password'] != '':
-        user.password = make_password(data['password'])
+    Supports updating user details and fetching current user information.
+    """
+    permission_classes = [IsAuthenticated]
 
-    user.save()
-    return Response(serializer.data)
+    def put(self, request):
+        user = request.user
+        data = request.data
 
+        try:
+            # Update user fields
+            user.first_name = data['first_name']
+            user.last_name = data['last_name']
+            user.email = data['email']
+            if data['password'] != '':
+                user.password = make_password(data['password'])
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_user(request):
-    user = request.user
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
+            user.save()
+            serializer = UserSerializerRefreshToken(user, many=False)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['POST'])
-def register_user(request):
-    data = request.data
-    try:
-        user = User.objects.create(
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            username=data['email'],
-            email=data['email'],
-            password=make_password(data['password'])
-        )
-
-        serializer = UserSerializerRefreshToken(user, many=False)
-        return Response(serializer.data)
-    except:
-        content = {'detail': 'This email is already associated with an account.'}
-        return Response(content, status=status.HTTP_409_CONFLICT)
+    def get(self, request):
+        try:
+            user = request.user
+            serializer = UserSerializer(user, many=False)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def get_users(request):
-    try:
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-    except:
-        content = {
-            'detail': 'Access Denied: This page is restricted to admin users only.'}
-        return Response(content, status=status.HTTP_403_FORBIDDEN)
+class AdminUserManagement(APIView):
+    """
+    API endpoint for admin to manage users.
+
+    Supports fetching all users, fetching a specific user, updating user details, and deleting users.
+    """
+    permission_classes = [IsAdminUser]
+
+    def get(self, request, pk=None):
+        try:
+            if pk:
+                user = get_object_or_404(User, id=pk)
+                serializer = UserSerializer(user, many=False)
+                return Response(serializer.data)
+            else:
+                users = User.objects.all()
+                serializer = UserSerializer(users, many=True)
+                return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, id=pk)
+        data = request.data
+
+        try:
+            # Update user fields
+            user.first_name = data['first_name']
+            user.last_name = data['last_name']
+            user.email = data['email']
+            user.is_staff = data['is_staff']
+            if data['password'] != '':
+                user.password = make_password(data['password'])
+
+            user.save()
+            serializer = UserSerializer(user, many=False)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, pk):
+        try:
+            user = get_object_or_404(User, id=pk)
+            user.delete()
+            return Response('User was deleted', status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@api_view(['DELETE'])
-@permission_classes([IsAdminUser])
-def delete_user(request, pk):
-    try:
-        user = User.objects.get(id=pk)
-        user.delete()
-        return Response('User was deleted', status=status.HTTP_200_OK)
-    except User.DoesNotExist:
-        return Response('User not found', status=status.HTTP_404_NOT_FOUND)
-    except Exception as e:
-        return Response(str(e), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class RegisterUser(APIView):
+    """
+    API endpoint for user registration.
 
+    Creates a new user account.
+    """
 
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def get_user_by_id(request, pk):
-    try:
-        user = User.objects.get(id=pk)
-        serializer = UserSerializer(user, many=False)
-        return Response(serializer.data)
-    except:
-        content = {
-            'detail': 'Access Denied: This page is restricted to admin users only.'}
-        return Response(content, status=status.HTTP_403_FORBIDDEN)
+    def post(self, request):
+        data = request.data
+        if User.objects.filter(email=data['email']).exists():
+            return Response({'detail': 'This email is already associated with an account.'}, status=status.HTTP_409_CONFLICT)
+        try:
+            user = User.objects.create(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                username=data['email'],
+                email=data['email'],
+                password=make_password(data['password'])
+            )
 
-
-@api_view(['PUT'])
-@permission_classes([IsAdminUser])
-def update_user_by_admin(request, pk):
-    user = User.objects.get(id=pk)
-    
-
-    data = request.data
-    user.first_name = data['first_name']
-    user.last_name = data['last_name']
-    user.email = data['email']
-    user.is_staff = data['is_staff']
-    if data['password'] != '':
-        user.password = make_password(data['password'])
-
-    user.save()
-    serializer = UserSerializer(user, many=False)
-    return Response(serializer.data)
+            serializer = UserSerializerRefreshToken(user, many=False)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': 'An error occurred during registration.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
