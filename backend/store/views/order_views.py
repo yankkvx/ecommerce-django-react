845927609a8600a2444796aef8e3a8e1e187c541
yankkv17 <yankkv17@gmail.com>
2024-06-28 19:23:57 +0300

    refactor: Update views to CBV for better structure and reusability
    Updated order_views.py to utilize Django REST frameworks class-based
    views for handling order creation, retrieval, and updates. Improved code
    readability and maintainability by using "APIView" for different user
    roles and error handling.

--- backend/store/views/order_views.py
+++ backend/store/views/order_views.py
@@ -1,119 +1,119 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from ..models import Product, Order, OrderItem, ShippingAddress
-from ..serializers import OrderSerializer
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from datetime import datetime
+from ..models import Product, Order, OrderItem, ShippingAddress
+from ..serializers import OrderSerializer
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def add_order_items(request):
-    user = request.user
-    data = request.data
-    # Extracts the order items from the data.
-    order_items = data['orderItems']
+class OrderManagement(APIView):
+    """
+    API endpoint for managing orders.
+    Supports creation, retrieval, and updating of orders.
+    Requires authentication for access.
+    """
+    permission_classes = [IsAuthenticated]
 
-    # Checks if there are no order items in the request data.
-    if order_items and len(order_items) == 0:
-        # Returns a response indicating no order items were provided, with a bad request status.
-        content = {'detail': 'No order items'}
-        return Response(content, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        # If there are order items, proceeds to create the order.
-        # Create a new order with the provided data.
-        order = Order.objects.create(
-            user=user,
-            payment_method=data['paymentMethod'],
-            shipping_price=data['shipping_price'],
-            total_price=data['total_price']
-        )
-        # Create a shipping address associated with the order.
-        shipping = ShippingAddress.objects.create(
-            order=order,
-            country=data['shippingAddress']['country'],
-            city=data['shippingAddress']['city'],
-            address=data['shippingAddress']['address'],
-            postal_code=data['shippingAddress']['postal_code'],
-        )
-        # Create order items and set the order to OrderItem relationship
-        for i in order_items:
-            product = Product.objects.get(id=i['product'])
-            product_image = product.productimage_set.first()
+    def post(self, request):
+        # Creates a new order with associated items and shipping address.
+        user = request.user
+        data = request.data
+        # Extracts the order items from the data.
+        order_items = data['orderItems']
 
-            # Creates the order item with the provided data.
-            item = OrderItem.objects.create(
-                product=product,
-                order=order,
-                name=product.name,
-                quantity=i['quantity'],
-                price=i['price'],
-                image=product_image,
+        # Checks if there are no order items in the request data.
+        if order_items and len(order_items) == 0:
+            # Returns a response indicating no order items were provided, with a bad request status.
+            return Response({'detail': 'No order items'}, status=status.HTTP_400_BAD_REQUEST)
+            # If there are order items, proceeds to create the order.
+            # Create a new order with the provided data.
+        try:
+            order = Order.objects.create(
+                user=user,
+                payment_method=data['paymentMethod'],
+                shipping_price=data['shipping_price'],
+                total_price=data['total_price']
             )
-            # Update the count_in_stock of the product after the order item is created.
-            product.count_in_stock -= item.quantity
-            product.save()
+            # Create a shipping address associated with the order.
+            shipping = ShippingAddress.objects.create(
+                order=order,
+                country=data['shippingAddress']['country'],
+                city=data['shippingAddress']['city'],
+                address=data['shippingAddress']['address'],
+                postal_code=data['shippingAddress']['postal_code'],
+            )
+            # Create order items and set the order to OrderItem relationship
+            for i in order_items:
+                product = get_object_or_404(Product, id=i['product'])
+                product_image = product.productimage_set.first()
 
-        serializer = OrderSerializer(order, many=False)
-        return Response(serializer.data)
+                # Creates the order item with the provided data.
+                item = OrderItem.objects.create(
+                    product=product,
+                    order=order,
+                    name=product.name,
+                    quantity=i['quantity'],
+                    price=i['price'],
+                    image=product_image,
+                )
+                # Update the count_in_stock of the product after the order item is created.
+                product.count_in_stock -= item.quantity
+                product.save()
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_order(request, pk):
-    user = request.user
-    order = Order.objects.get(id=pk)
-    try:
-        if user.is_staff or order.user == user:
             serializer = OrderSerializer(order, many=False)
             return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, pk=None):
+        # Retrieves orders based on user permissions.
+        user = request.user
+
+        if pk:
+            order = get_object_or_404(Order, id=pk)
+            if user.is_staff or order.user == user:
+                serializer = OrderSerializer(order, many=False)
+                return Response(serializer.data)
+            else:
+                return Response({'detail': 'Access denied. This order does not belong to you.'}, status=status.HTTP_403_FORBIDDEN)
         else:
-            content = {
-                'detail': 'Access denied. This order does not belong to you'}
-            return Response(content, status=status.HTTP_403_FORBIDDEN)
-    except:
-        content = {'detail': 'Order not found.'}
-        return Response(content, status=status.HTTP_404_NOT_FOUND)
+            orders = user.order_set.all()
+            serializer = OrderSerializer(orders, many=True)
+            return Response(serializer.data)
+
+    def put(self, request, pk):
+        order = get_object_or_404(Order, id=pk)
+        order.is_paid = True
+        order.paid_at = datetime.now()
+        order.save()
+
+        return Response('Order was paid.', status=status.HTTP_200_OK)
 
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def paid_order_update(request, pk):
-    order = Order.objects.get(id=pk)
-    order.is_paid = True
-    order.paid_at = datetime.now()
-    order.save()
+class AdminOrderManagement(APIView):
+    """
+    API endpoint for managing orders by admins.
+    Supports retrieval and updating of orders.
+    Requires admin permissions for access.
+    """
+    permission_classes = [IsAdminUser]
 
-    return Response('Order was paid')
+    def get(self, request):
+        # Retrieves all orders.
+        try:
+            orders = Order.objects.all()
+            serializer = OrderSerializer(orders, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk):
+        # Updates the delivery status of a specific order.
+        order = get_object_or_404(Order, id=pk)
+        order.is_delivered = True
+        order.delivered_at = datetime.now()
+        order.save()
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_user_orders(request):
-    user = request.user
-    orders = user.order_set.all()
-    serializer = OrderSerializer(orders, many='True')
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def get_orders(request):
-    try:
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-    except Exception as e:
-        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['PUT'])
-@permission_classes([IsAdminUser])
-def deliver_order_update(request, pk):
-    order = Order.objects.get(id=pk)
-    order.is_delivered = True
-    order.delivered_at = datetime.now()
-    order.save()
-
-    return Response('Order was delivered.', status=status.HTTP_200_OK)
+        return Response('Order was delivered.', status=status.HTTP_200_OK)
